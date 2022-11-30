91827d02680b1f3df5578c8976408aec27e12a95
kajaldoye <119444150+kajaldoye@users.noreply.github.com>
2022-11-30 17:52:19 +0530

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,4 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> for i in range(3,31,3):
+...     print(1)
